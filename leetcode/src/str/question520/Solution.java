6bda44c08d0ec17437106676e578174b344d10e9
Shen Zhengyu <18302010009@fudan.edu.cn>
2022-01-25 10:42:58 +0800

    update

---
+++ leetcode/src/str/question520/Solution.java
@@ -0,0 +1,39 @@
+package str.question520;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @author: Shen Zhengyu
+ * @create: 2022-01-24 08:30
+ **/
+public class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() == 1) return true;
+        char[] chars = word.toCharArray();
+        if (isCapital(chars[0])){
+            if (isCapital(chars[1])){
+                for (int i = 2; i < chars.length; i++) {
+                    if (!isCapital(chars[i])){
+                        return false;
+                    }
+                }
+            }else{
+                for (int i = 2; i < chars.length; i++) {
+                    if (isCapital(chars[i])){
+                        return false;
+                    }
+                }
+            }
+        }else{
+            for (int i = 1; i < chars.length; i++) {
+                if (isCapital(chars[i])){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public boolean isCapital(char c){
+        return  (c >= 'A' && c <= 'Z');
+    }
+}
