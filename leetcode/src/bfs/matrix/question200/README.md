e591973dc9e0066bee53362c9e61468485835c36
Shen Zhengyu <18302010009@fudan.edu.cn>
2022-02-09 11:56:56 +0800

    Solve question 200.

--- leetcode/src/bfs/matrix/question200/README.md
+++ leetcode/src/bfs/matrix/question200/README.md
@@ -4,3 +4,14 @@ Given an `m x n` 2D binary grid `grid` which represents a map of `'1'`s (land) a
 
 An **island** is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
 
+# DFS
+
+We can regard the matrix as an undirected graph.
+
+To count the islands, we can scan the two-dimension matrix, if a position is marked as '1', count++ and start depth first search from this position and mark every adjacent '1' as '0' to avoid duplicate count.
+
+The number of islands is the times we find that a position is marked as '1' cause it indicates that it is an individual, unfound island.
+
+# BFS
+
+If a position is marked as '1', add it to the queue and start breadth first search, mark and add to queue while the queue is not empty. Look for the next individual, unfound island after the queue is empty.
--- leetcode/src/bfs/matrix/question200/Solution.java
+++ leetcode/src/bfs/matrix/question200/Solution.java
@@ -1,5 +1,8 @@
 package bfs.matrix.question200;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @program: leetcode
  * @description:
@@ -7,13 +10,54 @@ package bfs.matrix.question200;
  * @create: 2022-02-08 21:12
  **/
 public class Solution {
+    public int bfsNumIslands(char[][] grid) {
+        if (null == grid || grid.length == 0) {
+            return 0;
+        }
+        int nor = grid.length;
+        int noc = grid[0].length;
+        int count = 0;
+        for (int r = 0; r < nor; r++) {
+            for (int c = 0; c < noc; c++) {
+                if ('1' == grid[r][c]) {
+                    count += 1;
+                    grid[r][c] = '0';
+                    Queue<Integer> neighbors = new LinkedList<Integer>();
+                    neighbors.add(r * noc + c);
+                    while (!neighbors.isEmpty()) {
+                        int id = neighbors.remove();
+                        int row = id / noc;
+                        int col = id % noc;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1') {
+                            neighbors.add((row - 1) * noc + col);
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 < nor && grid[row + 1][col] == '1') {
+                            neighbors.add((row + 1) * noc + col);
+                            grid[row + 1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1') {
+                            neighbors.add(row * noc + col - 1);
+                            grid[row][col - 1] = '0';
+                        }
+                        if (col + 1 < noc && grid[row][col + 1] == '1') {
+                            neighbors.add(row * noc + col + 1);
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
     public int numIslands(char[][] grid) {
         int count = 0;
         int nor = grid.length;
         int noc = grid[0].length;
         for (int i = 0; i < nor; i++) {
             for (int i1 = 0; i1 < noc; i1++) {
-                if (grid[i][i1] == '1'){
+                if (grid[i][i1] == '1') {
                     count++;
                     dfs(grid, i, i1);
                 }
