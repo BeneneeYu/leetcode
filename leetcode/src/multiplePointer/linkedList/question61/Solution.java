afcda7e037ee741eca9633b0272a12d8a2b39140
Shen Zhengyu <18302010009@fudan.edu.cn>
2022-03-11 09:03:30 +0800

    Solve question 61.

---
+++ leetcode/src/multiplePointer/linkedList/question61/Solution.java
@@ -0,0 +1,33 @@
+package multiplePointer.linkedList.question61;
+
+import multiplePointer.linkedList.ListNode;
+
+/**
+ * @program: leetcode
+ * @description:
+ * @author: Shen Zhengyu
+ * @create: 2022-03-11 08:51
+ **/
+public class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (null == head || null == head.next || k == 0) return head;
+        ListNode cur = head;
+        int n = 1;
+        while (null != cur.next) {
+            cur = cur.next;
+            n += 1;
+        }
+        int left = n - k % n;
+        if (left == n) return head;
+        // link as a cycle
+        cur.next = head;
+        while (left > 0) {
+            cur = cur.next;
+            left -= 1;
+        }
+        // find the new head and cur off
+        ListNode newHead = cur.next;
+        cur.next = null;
+        return newHead;
+    }
+}
