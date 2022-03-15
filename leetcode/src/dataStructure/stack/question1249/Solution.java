9d025daa9605af7dcf8e311e2c615fd93520c2ec
Shen Zhengyu <18302010009@fudan.edu.cn>
2022-03-15 10:55:50 +0800

    Solve question 1249.

--- leetcode/src/dataStructure/stack/question1249/Solution.java
+++ leetcode/src/dataStructure/stack/question1249/Solution.java
@@ -1,5 +1,9 @@
 package dataStructure.stack.question1249;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
 /**
  * @program: leetcode
  * @description:
@@ -7,4 +11,33 @@ package dataStructure.stack.question1249;
  * @create: 2022-02-23 19:27
  **/
 public class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Set<Integer> indexToRemove = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == '(') {
+                stack.push(i);
+            } else if (c == ')') {
+                if (stack.empty()) {
+                    indexToRemove.add(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        if (!stack.empty()) {
+            while (!stack.empty()) {
+                indexToRemove.add(stack.pop());
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            if (!indexToRemove.contains(i)) {
+                sb.append(chars[i]);
+            }
+        }
+        return sb.toString();
+    }
 }
