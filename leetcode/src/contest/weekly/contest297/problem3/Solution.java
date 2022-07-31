3b5bd05b22b76cb2989574e2b43bdb03bc200929
Shen Zhengyu <18302010009@fudan.edu.cn>
2022-07-31 12:51:28 +0800

    7.31

--- leetcode/src/contest/weekly/contest297/problem3/Solution.java
+++ leetcode/src/contest/weekly/contest297/problem3/Solution.java
@@ -4,10 +4,58 @@ import java.util.Arrays;
 
 /**
  * @program: Leetcode
- * @description:
+ * @description: You are given an integer array cookies, where cookies[i] denotes the number of cookies in the ith bag. You are also given an integer k that denotes the number of children to distribute all the bags of cookies to. All the cookies in the same bag must go to the same child and cannot be split up.
+ * <p>
+ * The unfairness of a distribution is defined as the maximum total cookies obtained by a single child in the distribution.
+ * <p>
+ * Return the minimum unfairness of all distributions.
  * @author: Shen Zhengyu
  * @create: 2022-06-12 10:25
  **/
 public class Solution {
+    int res = Integer.MAX_VALUE;
 
+    // k children
+    public int distributeCookies(int[] cookies, int k) {
+        dfs(cookies, 0, k, new int[k]);
+        return res;
+    }
+
+    private void dfs(int[] cookies, int cur, int k, int[] children) {
+        if (cur == cookies.length) {
+            System.out.println(Arrays.toString(children));
+            int max = Integer.MIN_VALUE;
+            for (int child : children) {
+                max = Math.max(max, child);
+            }
+            res = Math.min(res, max);
+            return;
+        }
+
+        // this bag of cookies, try to assign to every one
+        for (int i = 0; i < k; i++) {
+            children[i] += cookies[cur];
+            System.out.println(i + ":" + Arrays.toString(children));
+            dfs(cookies, cur + 1, k, children);
+            children[i] -= cookies[cur];
+        }
+    }
+
+    /* [8,15,10] 2
+0:[8, 0]
+0:[23, 0]
+0:[33, 0]
+1:[23, 10]
+1:[8, 15]
+// 15 assigned to child 2, 10 assigned 2 cases
+0:[18, 15]
+1:[8, 25]
+1:[0, 8]
+0:[15, 8]
+0:[25, 8]
+1:[15, 18]
+1:[0, 23]
+0:[10, 23]
+1:[0, 33]
+     */
 }
--- leetcode/src/contest/weekly/contest297/problem4/Solution.java
+++ leetcode/src/contest/weekly/contest297/problem4/Solution.java
@@ -1,5 +1,7 @@
 package contest.weekly.contest297.problem4;
 
+import java.util.HashSet;
+
 /**
  * @program: Leetcode
  * @description:
@@ -7,4 +9,23 @@ package contest.weekly.contest297.problem4;
  * @create: 2022-06-12 10:25
  **/
 public class Solution {
+    public long distinctNames(String[] ideas) {
+        HashSet<Integer>[] count = new HashSet[26];
+        for (int i = 0; i < 26; ++i)
+            count[i] = new HashSet<>();
+        for (String s : ideas)
+            count[s.charAt(0) - 'a'].add(s.substring(1).hashCode());
+        long res = 0;
+        // for any two characters (not counting duplicated)
+        for (int i = 0; i < 26; ++i)
+            for (int j = i + 1; j < 26; ++j) {
+                long c1 = 0, c2 = 0;
+                for (int c : count[i]) // all hashcode of postfix started with char_i
+                    if (!count[j].contains(c)) c1++; // a valid
+                for (int c : count[j])
+                    if (!count[i].contains(c)) c2++;
+                res += c1 * c2;
+            }
+        return res * 2; // pair, swapped names can be different
+    }
 }
